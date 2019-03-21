72f2f0c2a427de6bb4573c948b5f77fa32f1a381
Sergei Alimov <8912652@gmail.com>
2019-03-21 17:48:52 +0300

    add connection to mongoose

--- myApp.js
+++ myApp.js
@@ -12,7 +12,9 @@
 // `mongoose`. Store your **mLab** database URI in the private `.env` file 
 // as `MONGO_URI`. Connect to the database using `mongoose.connect(<Your URI>)`
 
-
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
+// mongodb+srv://sergei:<password>@cluster0-ahcy9.mongodb.net/test?retryWrites=true
 /** # SCHEMAS and MODELS #
 /*  ====================== */
 
